10d6f815102c129ca8765e9b5170ca3b91db5530
LuanaDG <luanadelagiustina@gmail.com>
2021-03-15 13:13:13 -0300

    modal

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-import { View, Text, ImageBackground, StyleSheet, FlatList } from 'react-native'
+import { View, Text, ImageBackground, StyleSheet, FlatList, TouchableOpacity, Platform } from 'react-native'
 import todayImage from '../../assets/assets/imgs/today.jpg'
 import moment from 'moment'
 import 'moment/locale/pt-br'
+import Icon from 'react-native-vector-icons/FontAwesome';
 import commonStyles from '../commonStyles'
 import Task from '../components/Task'
-
+import AddTask from './AddTask'
 
 
 //TELA COM A LISTA DAS TAREFAS
@@ -13,6 +14,9 @@ import Task from '../components/Task'
 export default class TaskList extends Component {
 
     state = {
+        showDoneTasks: true,
+        showAddTask: false,
+        visibleTasks: [],
         tasks: [{
             id: Math.random(),
             desc: 'Ir ao supermercado',
@@ -26,14 +30,53 @@ export default class TaskList extends Component {
         }]
     }
 
+    componentDidMount = () => {
+        this.filterTasks()
+    }
+
+    toggleFilter = () => {
+        this.setState({ showDoneTasks: !this.state.showDoneTasks }, this.filterTasks)
+    }
+
+    onCancel = () => {
+        this.setState({ showAddTask: false })
+    }
+
+    filterTasks = () => {
+        let visibleTasks = null
+        if (this.state.showDoneTasks) {
+            visibleTasks = [...this.state.tasks]
+        } else {
+            const pending = task => task.doneAt === null
+            visibleTasks = this.state.tasks.filter(pending)
+        }
+        this.setState({ visibleTasks })
+    }
+
+    toggleTask = taskId => {
+        const tasks = [...this.state.tasks]
+        tasks.forEach(task => {
+            if (task.id === taskId) {
+                task.doneAt = task.doneAt ? null : new Date()
+            }
+        })
+        this.setState({ tasks }, this.filterTasks)
+    }
+
     render() {
 
         const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
 
         return (
             <View style={styles.container}>
-                <ImageBackground source={todayImage}
-                    style={styles.background}>
+                <AddTask isVisible={this.state.showAddTask} onCancel={this.onCancel} />
+                <ImageBackground source={todayImage} style={styles.background}>
+                    <View style={styles.iconBar}>
+                        <TouchableOpacity onPress={this.toggleFilter}>
+                            <Icon name={this.state.showDoneTasks ? 'eye' : 'eye-slash'}
+                                size={20} color={commonStyles.colors.secondary} />
+                        </TouchableOpacity>
+                    </View>
                     <View style={styles.titleBar}>
                         <Text style={styles.title}>Hoje</Text>
                         <Text style={styles.subtitle}>{today}</Text>
@@ -41,11 +84,17 @@ export default class TaskList extends Component {
                 </ImageBackground>
 
                 <View style={styles.taskList}>
-                    <FlatList data={this.state.tasks}
+                    <FlatList data={this.state.visibleTasks}
                         keyExtractor={item => `${item.id}`}
-                        renderItem={({ item }) => <Task {...item} />}
+                        renderItem={({ item }) => <Task {...item} toggleTask={this.toggleTask} />}
                     />
                 </View>
+                <TouchableOpacity style={styles.addButton}
+                    activeOpacity={0.7}
+                    onPress={() => this.setState({ showAddTask: true })}>
+                    <Icon name="plus" size={20}
+                        color={commonStyles.colors.secondary} />
+                </TouchableOpacity>
             </View>
         )
     }
@@ -78,6 +127,23 @@ const styles = StyleSheet.create({
         color: commonStyles.colors.secondary,
         marginLeft: 20,
         marginBottom: 30
+    },
+    iconBar: {
+        flexDirection: 'row',
+        marginHorizontal: 20,
+        justifyContent: 'flex-end',
+        marginTop: Platform.OS === 'ios' ? 40 : 15
+    },
+    addButton: {
+        position: 'absolute',
+        right: 30,
+        bottom: 30,
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        backgroundColor: commonStyles.colors.today,
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 
 })
