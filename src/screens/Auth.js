3d1f6a0b1f57c1ab733132ba3ec7ebe4aebca26f
LuanaDG <luanadelagiustina@gmail.com>
2021-03-18 14:19:56 -0300

    autenticaçao com backend

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -3,26 +3,78 @@ import { ImageBackground, Text, StyleSheet, View, TouchableOpacity, Alert } from
 import backgroundImage from '../../assets/assets/imgs/login.jpg'
 import commonStyles from '../commonStyles'
 import AuthInput from '../components/AuthInput'
+import axios from 'axios'
+import { server, shoowError, showError, showSuccess } from '../common'
+
+
+
+const initialState = {
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    stageNew: false
+}
 
 export default class Auth extends Component {
 
     state = {
-        name: '',
-        email: '',
-        password: '',
-        confirmPassword: '',
-        stageNew: false
+        ...initialState
     }
 
     signinOrSignup = () => {
         if (this.state.stageNew) {
-            Alert.alert('Sucesso!', 'Criar Conta')
+            this.signup()
         } else {
-            Alert.alert('Sucesso!', 'Logar')
+            this.signin()
         }
     }
 
+    signup = async () => {
+        try {
+            await axios.post(`${server}/signup`, {
+                name: this.state.name,
+                email: this.state.email,
+                password: this.state.password,
+                confirmPassword: this.state.confirmPassword
+            })
+            showSuccess('Usuário cadastrado!')
+            this.setState({ ...initialState })
+
+        } catch (e) {
+            showError(e)
+        }
+    }
+
+    signin = async () => {
+        try {
+            const res = await axios.post(`${server}/signin`, {
+                email: this.state.email,
+                password: this.state.password,
+            })
+
+            axios.defaults.headers.common['Authorization'] = `bearer ${res.data.token}`
+            this.props.navigation.navigate('Home')
+        } catch (e) {
+            showError(e)
+        }
+    }
+
+
+
     render() {
+
+        const validations = []
+        validations.push(this.state.email && this.state.email.includes('@'))
+        validations.push(this.state.password && this.state.password.length >= 6)
+
+        if (this.state.stageNew) {
+            validations.push(this.state.name && this.state.name.trim().length >= 3)
+            validations.push(this.state.confirmPassword)
+            validations.push(this.state.password === this.state.confirmPassword)
+        }
+
+
         return (
             <ImageBackground source={backgroundImage} style={styles.background}>
                 <Text style={styles.title} >Tasks</Text>
